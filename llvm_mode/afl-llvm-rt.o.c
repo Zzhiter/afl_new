ecf9c3386def61c789559b7213ca0bbfb5434c11
hit_zz <2631992879@qq.com>
2023-09-24 13:00:09 +0800

    add instrument

--- llvm_mode/afl-llvm-rt.o.c
+++ llvm_mode/afl-llvm-rt.o.c
@@ -73,14 +73,14 @@ __thread u32 __afl_prev_loc;
 static u8 is_persistent;
 
 void __afl_gep_status(uint64_t id, uint64_t index) {
-  // if (id > 1900 || index > 1900) return;
+  if (id > 1900 || index > 1900) return;
   
-  // if (__afl_gep_status_ptr[id * 2000 + index] == 0) {
-  //   __afl_gep_status_ptr[id * 2000 + index] = 1;
-  //   (*__afl_gep_new_status_cnt) ++;
-  // }
+  if (__afl_gep_status_ptr[id * 2000 + index] == 0) {
+    __afl_gep_status_ptr[id * 2000 + index] = 1;
+    (*__afl_gep_new_status_cnt) ++;
+  }
 
-  *__afl_gep_new_status_cnt = 1;
+  // *__afl_gep_new_status_cnt = 1;
 
   // printf("xxx%d\n", __afl_gep_status_ptr[id * 2000 + index]);
 }
