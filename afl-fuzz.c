e2995c96920e72da0a8e72e462e403a422c977f3
Zzhiter <2631992879@qq.com>
2023-11-23 15:46:48 +0800

    fix: add logfile option

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -8004,7 +8004,7 @@ int main(int argc, char** argv) {
   gettimeofday(&tv, &tz);
   srandom(tv.tv_sec ^ tv.tv_usec ^ getpid());
 
-  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:Q")) > 0)
+  while ((opt = getopt(argc, argv, "+i:o:f:m:t:T:dnCB:S:M:x:QL")) > 0)
 
     switch (opt) {
 
